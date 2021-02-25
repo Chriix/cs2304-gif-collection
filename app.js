68ed9ce1e24927eedd9ba5400cca9d4871a5f02f
Chris Bonner <chrisbonner@ChrisBook-Air.local>
2021-02-25 13:45:44 -0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+const express = require("express");
+
+const MongoClient = require("mongodb").MongoClient;
+const MONGO_URL = "mongodb://mongo:27017"
+// Change this to your own greeting
+const MY_MESSAGE = "\"No, I'm Chrix!\"";
+
+// NOTE - add the next few lines
+MongoClient.connect(MONGO_URL, (err, db) => {
+  if (err) throw err;
+
+  console.log("Database has connected!");
+  const gifCollection = db.db("test").collection("gifs");
+
+  // Create our app and configure the view templating engine
+  const app = express();
+  app.set("view engine", "hbs");
+  app.set("views", __dirname + "/views");
+ 
+  // Add a handler for requests to "/". Simply picks a random image and renders the template.
+  app.get("/", async (req, res) => {
+    const cursor = gifCollection.aggregate([{ $sample: { size : 1 }}]);
+    const data = await cursor.next();
+    const imageUrl = (data) ? data.url : "https://media.giphy.com/media/14uQ3cOFteDaU/giphy.gif";
+    res.render("index", { imageUrl, message: MY_MESSAGE });
+  });
+
+  // Add body parsers so our app can read form inputs
+  app.use(express.urlencoded({
+    extended: true
+  }));
+
+  // Adds gif requested from user input
+  app.post("/add-gif", async (req, res) => {
+    const newDocument = { url : req.body.url, default: false };
+    await gifCollection.insertOne(newDocument);
+    res.redirect("/");
+  });
+  // Start the webserver on port 3000
+  app.listen(3000, () => console.log("Listening on port 3000"));
+
+  // Some nice cleanup handling to close things down when Docker stops the container
+  process.on("SIGINT", () => process.exit());
+  process.on("SIGTERM", () => process.exit());
+});
